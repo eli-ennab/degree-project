1a5d5490775bf0bb29550a41b058a3dd5a3763db
Eli Ennab <elinaennab@gmail.com>
2023-12-12 14:19:59 +0100

    init: typescript, next.js, react, prettier, eslint, readme

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Nioosha Shams',
+  description: 'The Digital Representation of Nioosha Shams',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="sv">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
