70d90d1963d39251f1fab11241c8104527d9ea24
Eli Ennab <elinaennab@gmail.com>
2023-12-14 11:15:42 +0100

    test vercel deploy

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -5,14 +5,18 @@ const getLandingPageData = async (lang: string) => {
   const version = process.env.STORYBLOK_DATA_VERSION
   const token = process.env.STORYBLOK_TOKEN
   const url = `https://api.storyblok.com/v2/cdn/stories/landing-page?version=${version}&token=${token}&language=${lang}`
-  let request = await fetch(url, { next: { revalidate: 10 } })
+  try {
+    let request = await fetch(url, { next: { revalidate: 10 } })
 
-  const storyData = await request.json()
-  const { nav_section, content } = storyData.story.content
+    const storyData = await request.json()
+    const { nav_section, content } = storyData.story.content
 
-  return {
-    nav_section: nav_section[0],
-    content: content[0],
+    return {
+      nav_section: nav_section[0],
+      content: content[0],
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
@@ -21,8 +25,8 @@ export default async function Home({ params: { lang } }: any) {
 
   return (
     <main>
-      <Navigation data={data.nav_section} lang={lang} />
-      <Content data={data.content} lang={lang} />
+      <Navigation data={data?.nav_section} lang={lang} />
+      <Content data={data?.content} lang={lang} />
     </main>
   )
 }
