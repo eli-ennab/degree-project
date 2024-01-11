26afea82c111451239215205b94a769b0c50e4ab
Eli Ennab <elinaennab@gmail.com>
2024-01-11 13:39:34 +0100

    console art for my github

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,6 +1,23 @@
 import { storyblokEditable, StoryblokComponent } from '@storyblok/react/rsc'
 
 export default function Page({ blok }: any) {
+  console.log(
+    `
+                                       
+  (                              )  
+( )\\(      (                ) ( /(  
+))((_)\    ))\\ (     (    ( /( )\\()) 
+/((_)((_)  /((_))\\ )  )\\ ) )(_)|(_)\  
+(_))| |(_) (_)) _(_/\\ _(_/(((_)_| |(_)
+/ -_) || | / -_) ' \\)) ' \\)) _\` | '_ \\ 
+\\___|_||_| \\___|_||_||_||_|\\__,_|_.__/ `
+  )
+
+  console.log(
+    '%cFind me https://github.com/eli-ennab',
+    'color: #022CA1; font-family:monospace; font-size: 12px'
+  )
+
   return (
     <main {...storyblokEditable(blok)}>
       {blok.body?.map((nestedBlok: any) => (
