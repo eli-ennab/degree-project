cf6b40f05143acf3a0646cb397b726628c7a27e7
eli-ennab <elinaennab@gmail.com>
2024-02-06 16:38:20 +0100

    remove unused meta data

--- app/[lang]/admin/layout.tsx
+++ app/[lang]/admin/layout.tsx
@@ -1,11 +1,6 @@
 import { AuthContextProvider } from '@/context/AuthContext'
 import AdminNavigation from '@/components/AdminNavigation/AdminNavigation'
 
-export const metadata = {
-  title: 'Nioosha Shams',
-  description: 'A Digital Representation of Nioosha Shams',
-}
-
 export default function AdminLayout({
   children,
 }: {
