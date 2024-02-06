7ac5686d2d801b53dea264140195653767a45e84
eli-ennab <elinaennab@gmail.com>
2024-02-06 10:45:00 +0100

    success! hydration error in deploy is gone

--- app/[lang]/[[...slug]]/page.tsx
+++ app/[lang]/[[...slug]]/page.tsx
@@ -3,14 +3,15 @@ import StoryblokStory from '@storyblok/react/story'
 
 export const dynamicParams = true
 
-export default async function Page({ params }: any) {
+export default async function Page({ params, preview }: any) {
   let lang = params.lang || 'sv'
   let slug = params.slug ? params.slug.join('/') : 'home'
 
+  const version = preview ? 'draft' : 'published'
+
   const storyblokApi = getStoryblokApi()
   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, {
-    // version: 'published',
-    version: 'draft',
+    version,
     cv: Math.random(),
     language: lang,
     resolve_links: 'url',
@@ -21,30 +22,4 @@ export default async function Page({ params }: any) {
       <StoryblokStory story={data.story} />
     </div>
   )
-}
-
-export async function generateStaticParams({ params }: any) {
-  const storyblokApi = getStoryblokApi()
-  let { data } = await storyblokApi.get('cdn/links/', {
-    version: 'published',
-    // version: 'draft',
-    resolve_links: 'url',
-  })
-  let paths: { slug: any; lang: any }[] = []
-  Object.keys(data.links).forEach((linkKey) => {
-    if (data.links[linkKey].is_folder) {
-      return
-    }
-    const slug = data.links[linkKey].slug
-
-    if (slug == 'home') {
-      return paths.push({ slug: ['/'], lang: params.lang })
-    }
-
-    let splittedSlug = slug.split('/')
-
-    paths.push({ slug: splittedSlug, lang: params.lang })
-  })
-
-  return paths
 }
